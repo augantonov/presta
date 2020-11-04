2d432f34229bb2b3b0bfd98de8048adb021ecc99
augantonov <68588785+augantonov@users.noreply.github.com>
2020-11-04 14:20:23 +0200

    Update menu-main.js

--- themes/azr/js/components/navigation/menu-main/menu-main.js
+++ themes/azr/js/components/navigation/menu-main/menu-main.js
@@ -17,15 +17,3 @@ function addToggler(name) {
   addToggler("phones");
   addToggler("schedule");
 })();
-
-for (e of document.querySelectorAll(".toggler")) {
-  let toggle_target_id = e.getAttribute("toggle-target");
-  let toggle_target = document.getElementById(toggle_target_id);
-  e.addEventListener('click', function() {
-    for (t of document.querySelectorAll(".toggle")) {
-    	if (t != toggle_target) {
-    		t.classList.remove('toggle-active');
-      }
-    }
-    toggle_target.classList.toggle('toggle-active');
-  });
