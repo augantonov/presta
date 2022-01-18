61e506971c1eacbe9362ba762f191d650c194afc
augantonov <68588785+augantonov@users.noreply.github.com>
2022-01-18 15:54:22 +0200

    disclaimer to langswitcher

--- modules/azrua_content/src/InstallHelper.php
+++ modules/azrua_content/src/InstallHelper.php
@@ -719,11 +719,11 @@ class InstallHelper implements ContainerInjectionInterface {
       'langcode' => 'en',
       'field_disclaimer' => [
         'value' => $data['field_disclaimer'],
-        'format' => 'basic_html',
+        'format' => 'full_html',
       ],
       'field_copyright' => [
-        'value' => '&copy; ' . date("Y") . ' ' . $data['field_copyright'],
-        'format' => 'basic_html',
+        'value' => ' . $data['field_copyright' '&copy; ' . date("Y") . '],
+        'format' => 'full_html',
       ],
     ];
     return $values;
